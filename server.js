70c97ed0f01cc17c5c175b2305f03ec4ac8b122f
unknown <wbezoui@gmail.com>
2022-10-31 21:33:59 +0100

    LINKI

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const {v4: uuidV4} = require('uuid')
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
 
+app.use('/peerjs', peerServer);
 app.set('view engine', 'ejs') // Tell Express we are using EJS
 app.use(express.static('public')) // Tell express to pull the client script from the public folder
 
