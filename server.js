fdceef34603a44032cb118ed57b81c3a1cf26f66
unknown <wbezoui@gmail.com>
2022-10-31 19:17:36 +0100

    LINKI

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 const path = require("path");
-const { uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 const PORT = process.env.PORT || 3000;
 
 app.use('/', peerServer);
