2a5dbaf1cc0852ee8f0ff286c3892400aec96359
unknown <wbezoui@gmail.com>
2022-10-31 21:26:21 +0100

    LINKI

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 app.get("/:roomID", (req, res) => {
     res.render('room', { roomID: req.params.roomID });
